f1978e3d6a89c55fe56f464890c9c480a03729a6
charathmathew <charathm@mail.gvsu.edu>
2019-03-01 06:15:33 -0500

    .

--- server.java
+++ server.java
@@ -1,56 +1,55 @@
-
 import java.io.*;
 import java.net.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.*;
 
 
-import java.net.*; 
-import java.io.*; 
+import java.net.*;
+import java.io.*;
 
-public class server {
+public class server{
 
 	    private static final int port = 2000;
-	    
-	    public static void main(String args[]) throws Exception 
-	    { 
+
+	    public static void main(String args[]) throws Exception
+	    {
 	    	  //Socket that waits for a request
 	        ServerSocket firstSocket = new ServerSocket(port);
 	        System.out.println("Waiting....");
 	        // infinite loop to constantly service clients
 	        while (true) {
-	            
+
 	            System.out.println("Waiting for a Command");
-	            
+
 	            //receive a connection from client
 	            Socket connectionSocket = firstSocket.accept();
-	            
+
 	            // handles individual ftp clientsls
 	            FTPHandler handler = new FTPHandler(connectionSocket);
-	            
+
 	            //Output Connection
 	            System.out.println("Connection Successed");
-	            
+
 	            // start thread
 	            handler.start();
 	        }
 	    }
-}    
+}
+
 //Reference: https://www.youtube.com/watch?v=T0BEAGWGImU&t=1196s
-//Allow class to handle multilple threads of execution 
+//Allow class to handle multilple threads of execution
  class FTPHandler extends Thread{
-	 
+
     // server file path
-    static final String serverFilePath ="." + File.separator +
+    static String serverFilePath ="." + File.separator +
         "server_files" + File.separator;
 	    static final String EOF = "!EOF!";
-	    
+
 	    // connection port
 	    int port;
-	    
+
 	    boolean quit = false;
-	    
+
 	    //tokenizer for fromClient
 	    StringTokenizer tokens;
 
@@ -68,62 +67,62 @@ public class server {
 
 	    //string sent from client
 	    String fromClient;
-	    
+
 	    //receive a connection
 		Socket connectionSocket;
-		
+
 		//Input reader
 		BufferedReader FromClient;
-		 
+
 		 public FTPHandler(Socket connectionSocket) throws Exception {
 		        this.connectionSocket = connectionSocket;
-		        //writes something client 
+		        //writes something client
 		        DataOutputStream   ToClient =
 		                new DataOutputStream(connectionSocket.getOutputStream());
-		        //reads from client 
+		        //reads from client
 		         FromClient = new BufferedReader(new
 		                InputStreamReader(connectionSocket.getInputStream()));
-		         
+
 		        System.out.println("FTP Client connected " + connectionSocket.getInetAddress() +
 		                " Port:" + connectionSocket.getPort());
 		    }
- 
+
 	    public void run() {
-	    
+
 	    	 boolean quit = true;
 				while(quit) {
-					try {	
-						//reads what was typed from client 
+					try {
+						//reads what was typed from client
 						fromClient = FromClient.readLine();
-						
+
 		                System.out.println("fromClient: " + fromClient);
 
 		                //breaks user's command into tokens
 		                tokens = new StringTokenizer(fromClient);
-		                
+
 		                //first line is the port number
 		                firstLine = tokens.nextToken();
-		                
+
 		                port = Integer.parseInt(firstLine);
-		                
+
 		                //second line is the command option
 		                clientCommand = tokens.nextToken();
 
 		                //handle each different command here
-		                
+
 		                switch(clientCommand.toUpperCase()) {
-		                
+
 		                case "LIST":
-		                	
+
 		                /* socket for data transfer*/
 				        Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
-				        
+
 				        //reads data to send to client
 				        DataOutputStream dataOutToClient =
 				                new DataOutputStream(dataSocket.getOutputStream());
 
-				        
-				        // store a list of files in an array 
+
+				        // store a list of files in an array
 				        File folder = new File(serverFilePath);
 				        File[] listOfFiles = folder.listFiles();
 
@@ -131,27 +130,28 @@ public class server {
 				        for (File myFile: listOfFiles){
 				            //checks if it is a file
 				            if (myFile.isFile()) {
-				            	
+
 				            	//server reads the files and sends them to clients
 				                dataOutToClient.writeUTF(myFile.getName());
 				            }
 				        }
 				        //close
-				        dataOutToClient.writeUTF(EOF);
+				        //dataOutToClient.writeUTF(EOF);
 				        dataSocket.close();
 				       break;
 		                case"RETRIEVE":
 		                	Socket dataSocket1 = new Socket(connectionSocket.getInetAddress(), port);
 		                	DataOutputStream dataOutToClient1 =
 					                new DataOutputStream(dataSocket1.getOutputStream());
-		                	
+
 		                	try{
-		                		
+
 		                		fileName = tokens.nextToken();
-			                	Path filePath = Paths.get(serverFilePath + fileName);
-			                	
-		                
-			                	dataOutToClient1.writeBytes(port + " " + clientCommand + " " + '\n');    
+												serverFilePath = FileSystems.getDefault().getPath("").toAbsolutePath() + "/server_files/" + fileName;
+			                	//Path filePath = Paths.get(serverFilePath + fileName);
+
+
+			                	dataOutToClient1.writeBytes(port + " " + clientCommand + " " + '\n');
 		                	      FileInputStream fis = new FileInputStream(serverFilePath);
 		                	      File sendFile = new File(serverFilePath.toString());
 		                	      byte[] byteArray = new byte [(int) sendFile.length()];
@@ -162,22 +162,33 @@ public class server {
 		                	    catch(Exception e){
 		                	      System.out.println(e);
 		                	    }
-		                	
-		                
-		                	
+
+
+
 		                	break;
-		                case"STORE":
+
+		                case "STORE":
+											byte[] buffer = new byte[6000];
+											try{
+												fileName = tokens.nextToken();
+												InputStream is = connectionSocket.getInputStream();
+												String path = FileSystems.getDefault().getPath("").toAbsolutePath() + "/server_files/" + fileName;
+												FileOutputStream fos = new FileOutputStream(path);
+												is.read(buffer, 0, buffer.length);
+												fos.write(buffer, 0, buffer.length);
+											}
+											catch(Exception e){
+												System.out.println("HERE");
+											}
 		                	break;
 		                case"QUIT":
+										System.exit(1);
 		                	break;
-				    
+
 				    }
 	    					}catch(Exception e) {
 
 					}
 				}
 	    }
- }  
-				
- 
- 
+ }
